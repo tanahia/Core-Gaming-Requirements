4352d1dafb30ac757c61646950274efe96758237
Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>
2025-09-11 11:53:03 +0100

    Added a moving script that skyrockets the cube up. Signed-off-by: Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>

---
+++ CoreGamingCode/Assets/MariAndrei_Rogojina/MAR_cubeControl.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MAR_cubeControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 1, 0);
+    }
+}
