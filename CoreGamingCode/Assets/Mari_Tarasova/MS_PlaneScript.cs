eb82ffa45a450f812382db2a007106734c06293f
wizmarin <sparkle.shrimp.prog@gmail.com>
2025-09-11 11:56:08 +0100

    Update of plane script
    Signed-off-by: wizmarin <sparkle.shrimp.prog@gmail.com>

--- CoreGamingCode/Assets/Mari_Tarasova/MS_PlaneScript.cs
+++ CoreGamingCode/Assets/Mari_Tarasova/MS_PlaneScript.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class MS_PlaneScript : MonoBehaviour
 {
+    // private float speed = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,8 @@ public class MS_PlaneScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // transform.position += speed * transform.forward * Time.deltaTime;
+
+        transform.position += new Vector3(0, 1, 0) * Time.deltaTime;
     }
 }
