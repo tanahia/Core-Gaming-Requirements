4e6692e60898e754a824f757be8ee3593dcd64c3
Student Jack O Mahoney (TL_KCOMP_B_Y2) <t00261288@mymtu.ie>
2025-10-15 16:51:53 +0100

    Signed-off-by: Student Jack O Mahoney (TL_KCOMP_B_Y2) <t00261288@mymtu.ie> Button Fuctionallity Mostly Completed

--- CoreGamingCode/Assets/Jack_Omahoney/Creating Button/ButtonManager.cs
+++ CoreGamingCode/Assets/Jack_Omahoney/Creating Button/ButtonManager.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 public class ButtonManager : MonoBehaviour
 {
     public Transform buttonTransformClone;
-    //public Transform canvasTransform;
+    public Transform canvasTransform;
+    public Sprite playbuttonSprite;
+    public Sprite QuitbuttonSprite;
 
     ButtonControlScript playButton, quitButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       Transform newButton =  Instantiate(buttonTransformClone);
+       Transform newButton =  Instantiate(buttonTransformClone,canvasTransform);
        playButton = newButton.GetComponent<ButtonControlScript>();
-        playButton.Initilise();
-         playButton.SetButtonText("Play");
-         playButton.SetPosition(new Vector3(0, 50, 0));
-         playButton.SetColors(Color.white, Color.gray, Color.green, Color.black);
-
+       playButton.Initilise();
+       playButton.SetButtonText("Play");
+       playButton.SetPosition(new Vector2(0,0));
+       playButton.SetColors(Color.white, Color.gray, Color.green, Color.black);
+       playButton.SetButtonSize(150,50);
+       playButton.SetButtonImage(playbuttonSprite);
+        
+       newButton = Instantiate(buttonTransformClone,canvasTransform);
+       quitButton = newButton.GetComponent<ButtonControlScript>();
+       quitButton.Initilise();
+       quitButton.SetButtonText("Quit");
+       quitButton.SetPosition(new Vector2(0,-50));
+       quitButton.SetColors(Color.white, Color.gray, Color.red, Color.black);
+       quitButton.SetButtonSize(150,50);
+       quitButton.SetButtonImage(QuitbuttonSprite);
     }
 
     // Update is called once per frame
