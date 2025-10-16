70978dfa77beaf8d63b36c9bc8dfe095703514bb
Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>
2025-10-16 12:48:07 +0100

    Signed-off-by: Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>

--- CoreGamingCode/Assets/MariAndrei_Rogojina/CameraFPSTest/ControlFPS.cs
+++ CoreGamingCode/Assets/MariAndrei_Rogojina/CameraFPSTest/ControlFPS.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 
-public class TestingFPS : MonoBehaviour
+public class ControlFPS : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         YB_CameraControlFPS cam = transform.GetComponent<YB_CameraControlFPS>();
-        cam.RotateHorizontal(90f);
-        cam.RotateVertical(90f);
+        cam.RotateVertical(1);
+        cam.RotateHorizontal(1);
+        cam.SetCameraAngleLimit(-80f, 80f);
     }
 
     // Update is called once per frame
