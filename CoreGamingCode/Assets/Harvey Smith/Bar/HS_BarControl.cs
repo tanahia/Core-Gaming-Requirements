4d22a2ab1c186e1c03f0078a88ba5c46cea53e39
Student Harvey Smith (TL_KCOMP_B_Y2) <t00264389@mymtu.ie>
2025-10-16 12:48:40 +0100

    Tested bar, changed some methods and added documentation Signed-off-by: Student Harvey Smith (TL_KCOMP_B_Y2) <t00264389@mymtu.ie>

--- CoreGamingCode/Assets/Harvey Smith/Bar/HS_BarControl.cs
+++ CoreGamingCode/Assets/Harvey Smith/Bar/HS_BarControl.cs
@@ -4,79 +4,86 @@ using UnityEngine.UI;
 
 public class HS_BarControl : MonoBehaviour
 {
-    public Slider slider;
-    public Image image;
-    public RectTransform rectTransform;
-
+    Slider slider;
+    RectTransform rectTransform;
+    Image image;
+    bool initialised = false;
 
 
     void Start()
     {
-        if (slider != null)
-        {
-            SetBarPosition(new Vector2(-300, -220));
-        }
+        initialize();
     }
 
-        
+    private void initialize()
+    {
+        slider = GetComponent<Slider>();
+        if (slider) print("Slider Success");
+        Image[] imgs = GetComponentsInChildren<Image>();
+        image = imgs[1];
 
-        // Update is called once per frame
-        void Update()
+        rectTransform = GetComponent<RectTransform>();
+        if (rectTransform) print("RectTransform Success");
+        initialised = true;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
         {
-            BarColourChange();
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            SetBarValue(30.5);
-        }
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            SetBarValue(90);
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-           IncrementBar(10);
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            ReduceBar(10);
-        }
+      
 
 
     }
-    
+
 
     /// <summary>
-    /// Allows a health bars colour to 
-    /// change between green, yellow and red, 
-    /// depending on the sliders value
+    /// This method changes the colour of the bar, to a specified colour at a specified value
     /// </summary>
-    public void BarColourChange()
+    /// <param name="fullHealth">The value of which the full health colour stops</param>
+    /// <param name="middleHealth">The value of which the medium health stops</param>
+    /// <param name="fullHealthColor">The colour when above or equal to the full health value</param>
+    /// <param name="middleHealthColor">The colour when above or equal to the middle health value</param>
+    /// <param name="lowHealthColor">The colour when below the middle health value</param>
+    public void BarColourChange(double fullHealth, double middleHealth, Color fullHealthColor, Color middleHealthColor, Color lowHealthColor)
     {
-        float percentage = slider.value;
-        int fullHealth = 65;
-        int midHealth = 30;
+        if (!initialised) initialize();
 
-        //changes the bar colour to green, when between
+        float percentage = slider.value;
+        
+
+        //changes the bar colour to the specified color, when between
         //the full health margin, and the full bar
         if (percentage >= fullHealth)
         {
-            image.color = Color.green;
+            image.color = fullHealthColor;
         }
 
-        //changes the bar colour to yellow when below
+        //changes the bar colour to the specified color when below
         //the full health margin, but not below the mid
         //health margin
-        if (percentage < fullHealth && percentage >= midHealth)
+        if (percentage < fullHealth && percentage >= middleHealth)
         {
-            image.color = Color.yellow;
+            image.color = middleHealthColor;
         }
 
-        //changes the bar colour to red when below the
+        //changes the bar colour to the specified color when below the
         //mid health margin
-        if (percentage < midHealth)
+        if (percentage < middleHealth)
         {
-            image.color = Color.red;
+            image.color = lowHealthColor;
         }
+    }
+
+    /// <summary>
+    /// Changes the colour of the bar (No changing)
+    /// </summary>
+    /// <param name="color">The desired colour</param>
+    public void BarColour(Color color)
+    {
+        if (!initialised) initialize();
+        image.color = color;
     }
     /// <summary>
     /// Sets the bars position to the postion given in the parameter
@@ -84,11 +91,10 @@ public class HS_BarControl : MonoBehaviour
     /// <param name="position">position of object as a Vector2</param>
     public void SetBarPosition(Vector2 position)
     {
-        if (rectTransform != null)
-        {
-            rectTransform.anchoredPosition = position;
-        }
+        if (!initialised) initialize();
 
+ 
+        rectTransform.anchoredPosition = position;
     }
     /// <summary>
     /// Sets the bars value to a specified value
@@ -96,6 +102,7 @@ public class HS_BarControl : MonoBehaviour
     /// <param name="value">The given value as a double</param>
     public void SetBarValue(double value)
     {
+        if (!initialised) initialize();
         slider.value = (float)value;
     }
     /// <summary>
@@ -104,6 +111,7 @@ public class HS_BarControl : MonoBehaviour
     /// <param name="value">The given value that the bar will increase by as a double</param>
     public void IncrementBar(double value)
     {
+        if (!initialised) initialize();
         slider.value += (float)value;
     }
 
@@ -113,6 +121,7 @@ public class HS_BarControl : MonoBehaviour
     /// <param name="value">The given value that the bar will decrease by as a double</param>
     public void ReduceBar(double value)
     {
+        if (!initialised) initialize();
         slider.value -= (float)value;
     }
 
