f37fbe29abc506fa9f1da045b95f2505050e37fc
tanahia <sofi.bookfan@gmail.com>
2025-12-08 00:15:01 +0000

    Finished minigame
    Signed-off-by: tanahia <sofi.bookfan@gmail.com>

--- CoreGamingCode/Assets/SofiiaFedchuk/MiniGame/SF_MenuScript.cs
+++ CoreGamingCode/Assets/SofiiaFedchuk/MiniGame/SF_MenuScript.cs
@@ -8,7 +8,6 @@ public class SF_MenuScript : MonoBehaviour
     public Transform buttonTransformClone;
     public Transform canvasTransform;
     public Sprite playbuttonSprite;
-    public Animator transition1;
  
    public float transitionTime;
     bool isButtonFading = false;
@@ -23,22 +22,15 @@ public class SF_MenuScript : MonoBehaviour
         playButton.Initilise();
         playButton.SetButtonText("Play");
         playButton.SetPosition(new Vector2(0, 0));
-      //  playButton.SetColors(Color.white, Color.gray, Color.white, Color.black);
         playButton.SetButtonSize(150, 50);
         playButton.SetButtonImage(playbuttonSprite);
         playButton.SetButtonAction(LoadNextLevel);
-      // playButton.SetColors(new Color(1, 0, 0, 0.5f), Color.gray, Color.green, Color.black);
 
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-       
-        if(isButtonFading)
-        {
-            print(buttonOpacity);
-        }
        
     }
    
@@ -59,12 +51,9 @@ public class SF_MenuScript : MonoBehaviour
             buttonOpacity = Mathf.Lerp(1f, 0f, timer / transitionTime);
             canvasTransform.GetComponent<CanvasGroup>().alpha = buttonOpacity;
             yield return null;
-        }
-
-        transition1.SetTrigger("End");
-        //transition2.SetTrigger("ButtonEnd");
-       
+        }  
         yield return new WaitForSeconds(transitionTime);
-         SceneManager.LoadSceneAsync(name);
+         SceneManager.LoadScene(name);
+        print(timer);
      }
 }
